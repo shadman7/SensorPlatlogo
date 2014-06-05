51d872ecf7115d3e61dc64c8b55d90a839594178
Salim Shadman <salimshadman2@gmail.com>
2014-06-05 22:14:27 +0530

    Update SensorPlatlogo.java

--- src/com/shadman/sensor/platlogo/SensorPlatlogo.java
+++ src/com/shadman/sensor/platlogo/SensorPlatlogo.java
@@ -10,16 +10,15 @@ import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+//This is The Source Code of Sensor Platlogo You Can Modify This is because 
+//i am huge fan's  open source but give me good credits and don't forget to
+//follow me on github and google plus https://plus.google.com/109164564486633147222/about
+//Give Star on This Repotsipers.
 public class SensorPlatlogo extends Activity implements SensorEventListener {
 
-    // define the display assembly compass picture
     private ImageView image;
-
-    // record the compass picture angle turned
     private float currentDegree = 0f;
 
-    // device sensor manager
     private SensorManager mSensorManager;
 
     TextView tvHeading;
@@ -28,22 +27,15 @@ public class SensorPlatlogo extends Activity implements SensorEventListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        // our compass image 
         image = (ImageView) findViewById(R.id.imageViewCompass);
 
-        // TextView that will tell the user what degree is he heading
         tvHeading = (TextView) findViewById(R.id.tvHeading);
-
-        // initialize your android device sensor capabilities
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        
-        // for the system's orientation sensor registered listeners
         mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
                 SensorManager.SENSOR_DELAY_GAME);
     }
@@ -51,34 +43,22 @@ public class SensorPlatlogo extends Activity implements SensorEventListener {
     @Override
     protected void onPause() {
         super.onPause();
-        
-        // to stop the listener and save battery
         mSensorManager.unregisterListener(this);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-
-        // get the angle around the z-axis rotated
         float degree = Math.round(event.values[0]);
 
         tvHeading.setText("Heading: " + Float.toString(degree) + " degrees");
-
-        // create a rotation animation (reverse turn degree degrees)
         RotateAnimation ra = new RotateAnimation(
                 currentDegree, 
                 -degree,
                 Animation.RELATIVE_TO_SELF, 0.5f, 
                 Animation.RELATIVE_TO_SELF,
                 0.5f);
-
-        // how long the animation will take place
         ra.setDuration(210);
-
-        // set the animation after the end of the reservation status
         ra.setFillAfter(true);
-
-        // Start the animation
         image.startAnimation(ra);
         currentDegree = -degree;
 
@@ -86,6 +66,5 @@ public class SensorPlatlogo extends Activity implements SensorEventListener {
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // not in use
     }
 }
